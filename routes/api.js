d2d2d5674e47593bd3383a1ddf93fe1d7a511602
Mauro Walker <mauro@wolfsoft.solutions>
2024-06-11 14:15:52 +0100

    Added the writers route to the api file

--- routes/api.js
+++ routes/api.js
@@ -7,10 +7,12 @@ var router = express.Router();
 var usersRouter = require('./API/users');
 var contactRouter = require('./API/contact');
 var booksRouter = require('./API/books');
+var writersRouter = require('./API/writers');
 
 router.use('/users', usersRouter);
 router.use('/contact', contactRouter);
 router.use('/books', booksRouter);
+router.use('/writers', writersRouter);
 
 router.get('/', function(req, res, next){
     res.json({
