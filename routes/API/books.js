01ac976e31de70a95717b4c2cb5193da0e29db96
Mauro Walker <mauro@wolfsoft.solutions>
2024-06-11 11:55:04 +0100

    Split the books/add into multiple for image purposes.

--- routes/API/books.js
+++ routes/API/books.js
@@ -5,10 +5,6 @@ var multer = require("multer");
 var path = require("path");
 var fs = require("fs");
 
-// const uploadFolder = multer({
-//     dest: "./public/images/uploads/",
-// });
-
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       const uploadPath = path.join(__dirname, '../../public/images/uploads/');
@@ -34,13 +30,14 @@ router.get("/", async function (req, res) {
     }
 });
 
-router.post("/add", upload.single("image"), async function (req, res) {
+router.post("/add/cover_image", upload.single("image"), async function (req, res) {
     try {
         if(!req.file){
             console.log("No file uploaded")
             return res.status(400).send({ status: 400, message: "No file uploaded" });
         }
-        res.json({ message: "Book added successfully! I think", path: req.file.path.split("/public/")[1] });
+        let filePath = req.file.path.split("/public/")[1] || req.file.path.split("\\public\\")[1].replace(/\\/g, "/");
+        res.json({ message: "Book added successfully! I think", path: "/"+filePath });
     } catch (err) {
         res.status(500).json({ message: "An error occurred" });
         send_error(err, res);
