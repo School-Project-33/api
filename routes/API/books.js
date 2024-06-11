700f1460098646cd7d6f26fff3ab0777c179d9dc
Mauro Walker <mauro@wolfsoft.solutions>
2024-06-11 14:14:03 +0100

    Merged the images and book thing in one again for easier use. Not done yet

--- routes/API/books.js
+++ routes/API/books.js
@@ -4,6 +4,7 @@ var { send_error } = require("../../functions/error");
 var multer = require("multer");
 var path = require("path");
 var fs = require("fs");
+var { check_user_token } = require("../../functions/middleware");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -12,7 +13,7 @@ const storage = multer.diskStorage({
       cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
-      cb(null, Date.now() + path.extname(file.originalname)); // Append current timestamp to the file name
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // Append current timestamp to the file name
     }
 });
 
@@ -30,18 +31,28 @@ router.get("/", async function (req, res) {
     }
 });
 
-router.post("/add/cover_image", upload.single("image"), async function (req, res) {
+// router.post("/add/cover_image", upload.single("image"), async function (req, res) {
+//     try {
+//         if(!req.file){
+//             console.log("No file uploaded")
+//             return res.status(400).send({ status: 400, message: "No file uploaded" });
+//         }
+//         let filePath = req.file.path.split("/public/")[1] || req.file.path.split("\\public\\")[1].replace(/\\/g, "/");
+//         res.json({ message: "Book added successfully! I think", path: "/"+filePath });
+//     } catch (err) {
+//         res.status(500).json({ message: "An error occurred" });
+//         send_error(err, res);
+//     }
+// });
+
+router.post("/add", upload.fields([{ name: 'cover_image', maxCount: 1, encoding: '' }, { name: 'book_image', maxCount: 10 }]), async function (req, res) {
     try {
-        if(!req.file){
-            console.log("No file uploaded")
-            return res.status(400).send({ status: 400, message: "No file uploaded" });
-        }
-        let filePath = req.file.path.split("/public/")[1] || req.file.path.split("\\public\\")[1].replace(/\\/g, "/");
-        res.json({ message: "Book added successfully! I think", path: "/"+filePath });
-    } catch (err) {
-        res.status(500).json({ message: "An error occurred" });
-        send_error(err, res);
-    }
+        let { title, author, description, genre, price, stock, pages, publisher, published_date, isbn } = req.body;
+        let cover_image = req.files.cover_image[0].path.split("/public/")[1] || req.files.cover_image[0].path.split("\\public\\")[1].replace(/\\/g, "/");
+        let book_image = req.files.book_image.map(file => file.path.split("/public/")[1] || file.path.split("\\public\\")[1].replace(/\\/g, "/"));
+    } catch (e) {
+        send_error(e, res);
+    };
 });
 
 module.exports = router;
