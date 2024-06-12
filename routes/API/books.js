477387a387f3893032430cb5109e143acb876db6
HowlingArcher <mauro@wolfsoft.solutions>
2024-06-12 10:06:28 +0200

    upload multiple files works now

--- routes/API/books.js
+++ routes/API/books.js
@@ -8,16 +8,16 @@ var { check_user_token } = require("../../functions/middleware");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      const uploadPath = path.join(__dirname, '../../public/images/uploads/');
-      fs.mkdirSync(uploadPath, { recursive: true }); // Ensure the uploads directory exists
-      cb(null, uploadPath);
+        const uploadPath = path.join(__dirname, '../../public/images/uploads/');
+        fs.mkdirSync(uploadPath, { recursive: true }); // Ensure the uploads directory exists
+        cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // Append current timestamp to the file name
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // Append current timestamp to the file name
     }
 });
 
-var upload = multer({ storage });
+const upload = multer({ storage: storage });
 
 // create the router
 var router = express.Router();
@@ -45,14 +45,18 @@ router.get("/", async function (req, res) {
 //     }
 // });
 
-router.post("/add", upload.fields([{ name: 'cover_image', maxCount: 1, encoding: '' }, { name: 'book_image', maxCount: 10 }]), async function (req, res) {
+router.post('/add', upload.fields([
+    { name: 'cover_image', maxCount: 1 },
+    { name: 'book_image', maxCount: 10 }
+]), async function (req, res) {
     try {
-        let { title, author, description, genre, price, stock, pages, publisher, published_date, isbn } = req.body;
-        let cover_image = req.files.cover_image[0].path.split("/public/")[1] || req.files.cover_image[0].path.split("\\public\\")[1].replace(/\\/g, "/");
-        let book_image = req.files.book_image.map(file => file.path.split("/public/")[1] || file.path.split("\\public\\")[1].replace(/\\/g, "/"));
+        let coverImageFilePath = req.files.cover_image[0].path.split("/public/")[1] || req.files.cover_image[0].path.split("\\public\\")[1].replace(/\\/g, "/");
+        let bookImagesFilePath = req.files.book_image.map(file => file.path.split("/public/")[1] || file.path.split("\\public\\")[1].replace(/\\/g, "/"));
+        res.status(200).json({ status: 200, message: 'success', paths: {cover_image: coverImageFilePath, book_images: bookImagesFilePath} });
     } catch (e) {
-        send_error(e, res);
-    };
+        console.error('Error:', e);
+        res.status(500).json({ status: 500, message: 'Internal Server Error' });
+    }
 });
 
 module.exports = router;
