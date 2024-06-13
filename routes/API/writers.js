a686d54765f4a86066c01d93a88c2a8789bc22e0
HowlingArcher <mauro@wolfsoft.solutions>
2024-06-13 13:50:38 +0200

    Removed getting settings from writers

--- routes/API/writers.js
+++ routes/API/writers.js
@@ -9,7 +9,17 @@ var router = express.Router();
 router.get('/', async function(req, res, next){
     try {
         let writers = await query("SELECT * FROM writers");
-        res.json({writers: writers});
+        // also get the users first and last_name from the users table and add it to the writers object
+        for (let i = 0; i < writers.length; i++){
+            let user = await query("SELECT first_name, last_name FROM users WHERE id = ?", [writers[i].user_id]);
+            writers[i].first_name = user[0].first_name;
+            writers[i].last_name = user[0].last_name;
+        }
+        if(writers.length > 0){
+            await res.json({status: 200, message: "Success!", writers: writers});
+        } else{
+            res.status(404).json({status: 404, message: "Writers not found", writers: []});
+        };
     } catch (err) {
         res.status(500).json({status: 500, message: "Internal Server Error"});
         send_error(err, "Getting writers failed");
@@ -19,10 +29,14 @@ router.get('/', async function(req, res, next){
 router.get('/:id', async function(req, res, next){
     try {
         let writer = await query("SELECT * FROM writers WHERE id = ?", [req.params.id]);
+        // also get the users first and last_name from the users table and add it to the writers object
+        let user = await query("SELECT first_name, last_name FROM users WHERE id = ?", [writer[0].user_id]);
+        writer[0].first_name = user[0].first_name;
+        writer[0].last_name = user[0].last_name;
         if(writer.length > 0){
-            res.json({writer: writer[0]});
+            res.json({status: 200, message: "Success!", writer: writer[0]});
         } else{
-            res.status(404).json({status: 404, message: "Writer not found"});
+            res.status(404).json({status: 404, message: "Writer not found", writers: []});
         };
     } catch (err) {
         res.status(500).json({status: 500, message: "Internal Server Error"});
@@ -48,21 +62,6 @@ router.post('/settings/:id', check_user_token, check_user_id, async function(req
     } catch (err) {
         res.status(500).json({status: 500, message: "Internal Server Error"});
         send_error(err, "Updating writer settings failed");
-    }
-});
-
-router.get('/settings/:id', async function(req, res) {
-    try {
-        let writer = await query("SELECT * FROM writers WHERE id = ?", [req.params.id]);
-        if(writer.length > 0){
-            let settings = JSON.parse(writer[0].settings);
-            res.json({settings: settings});
-        } else{
-            res.status(404).json({status: 404, message: "Writer not found"});
-        };
-    } catch (err) {
-        res.status(500).json({status: 500, message: "Internal Server Error"});
-        send_error(err, "Getting writer settings failed");
     }
 });
 
