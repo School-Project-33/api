57ce1ab12ac074e6002f765f498bef8dfa40f040
Mauro Walker <mauro@wolfsoft.solutions>
2024-06-15 14:13:38 +0100

    Made swagger docs for uploading a book

--- routes/API/books.js
+++ routes/API/books.js
@@ -4,11 +4,11 @@ var { send_error } = require("../../functions/error");
 var multer = require("multer");
 var path = require("path");
 var fs = require("fs");
-var { check_user_token } = require("../../functions/middleware");
+var { check_user_token, isSeller } = require("../../functions/middleware");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        const uploadPath = path.join(__dirname, '../../public/images/uploads/');
+        const uploadPath = path.join(__dirname, '../../public/images/uploads/'+req.user.first_name+"/"+req.body.title);
         fs.mkdirSync(uploadPath, { recursive: true }); // Ensure the uploads directory exists
         cb(null, uploadPath);
     },
@@ -31,15 +31,23 @@ router.get("/", async function (req, res) {
     }
 });
 
-router.post('/add', upload.fields([
+
+router.post('/add', check_user_token, isSeller, upload.fields([
     { name: 'cover_image', maxCount: 1 },
-    { name: 'book_image', maxCount: 10 }
+    { name: 'book_images', maxCount: 10 }
 ]), async function (req, res) {
     try {
-        console.log(req.body.requestData);
+        console.log(req.body);
+        
         let coverImageFilePath = req.files.cover_image[0].path.split("/public/")[1] || req.files.cover_image[0].path.split("\\public\\")[1].replace(/\\/g, "/");
-        let bookImagesFilePath = req.files.book_image.map(file => file.path.split("/public/")[1] || file.path.split("\\public\\")[1].replace(/\\/g, "/"));
-        res.status(200).json({ status: 200, message: 'success', paths: {cover_image: coverImageFilePath, book_images: bookImagesFilePath} });
+        
+        // Check if req.files.book_image exists before mapping
+        let bookImagesFilePath = [];
+        if (req.files.book_image) {
+            bookImagesFilePath = req.files.book_image.map(file => file.path.split("/public/")[1] || file.path.split("\\public\\")[1].replace(/\\/g, "/"));
+        }
+        
+        res.status(200).json({ status: 200, message: 'success', paths: { cover_image: coverImageFilePath, book_images: bookImagesFilePath } });
     } catch (e) {
         console.error('Error:', e);
         res.status(500).json({ status: 500, message: 'Internal Server Error' });
