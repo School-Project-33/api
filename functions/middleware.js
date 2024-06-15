c3c043fe29898d32d050894a2f998984f2a2e84e
Mauro Walker <mauro@wolfsoft.solutions>
2024-06-15 13:20:01 +0100

    Fixed the middleware

--- functions/middleware.js
+++ functions/middleware.js
@@ -1,9 +1,5 @@
-let db = require("../db");
-let { query } = require("./database_queries");
-
 async function check_user_token(req, res, next) {
     var webToken = req.headers['authorization'];
-    console.log("Authorization header:", webToken);
 
     if (webToken) {
         if (webToken.startsWith("Bearer ")) {
@@ -13,10 +9,7 @@ async function check_user_token(req, res, next) {
         }
     }
 
-    console.log("Extracted token:", webToken);
-
     if (webToken === undefined) {
-        console.log("No token provided or token format incorrect");
         res.status(401).send({ "status": 401, "message": "Invalid token" });
     } else {
         db.query("SELECT id,first_name,last_name,email,email_verified,phone_number,role,seller FROM users WHERE token = ?", [webToken], function (err, result) {
@@ -30,52 +23,73 @@ async function check_user_token(req, res, next) {
                 let token_expires_at = new Date(result[0].token_expires_at);
                 let now = new Date();
                 if (token_expires_at < now) {
-                    console.log("Token expired");
                     res.status(401).json({ "status": 401, "message": "Token expired" });
                 } else {
                     req.user = result[0];
                     next();
                 }
             } else {
-                console.log("Token not found in database");
                 res.status(401).send({ "status": 401, "message": "Invalid token" });
             }
         });
     }
 }
 
-async function check_user_id() {
-	return function(req, res, next) {
-		let user_id = req.params.id;
-		var webToken = req.headers['authorization'];
-		if(webToken){
-			if(webToken.startsWith("Bearer ")){
-				webToken = webToken.split(" ")[1];
-			} else {
-				webToken = undefined;
-			};
-		};
-	
-		if (webToken === undefined){
-			res.status(401).send({"status": 401, "message": "Invalid token"});
-		} else{
-			db.query("SELECT * FROM users WHERE token = ?", [webToken], function(err, result){
-				if (err) throw err;
-				if (result.length > 0){
-					if (result[0].id === user_id){
-						next();
-					} else{
-						res.status(401).send({"status": 401, "message": "Unauthorized"});
-					};
+async function check_user_id(req, res, next) {
+	let user_id = req.params.id;
+	if (req.user.id === user_id) {
+		next();
+	} else {
+		res.status(401).send({"status": 401, "message": "Unauthorized"});
+	}
+}
+
+function check_writer_id(req, res, next) {
+    let id = req.params.id;
+    let user_id = req.user.id;
+    let seller = req.user.seller;
+    
+    if (!seller) {
+        return res.status(401).send({"status": 401, "message": "Unauthorized"});
+    }
+
+    db.query("SELECT user_id FROM writers WHERE id = ?", [id], function(err, result) {
+        if (err) {
+            return next(err);
+        }
+        if (result.length > 0) {
+            if (result[0].user_id === user_id || seller === 1) {
+                next();
+            } else {
+                res.status(401).send({"status": 401, "message": "Unauthorized"});
+            }
+        } else {
+            res.status(404).send({"status": 404, "message": "Writer not found"});
+        }
+    });
+}
+
+async function isSeller(){
+	return function(req, res, next){
+		let user_id = req.user.id;
+		db.query("SELECT seller FROM users WHERE id = ?", [user_id], function(err, result){
+			if (err) throw err;
+			if (result.length > 0){
+				if (result[0].seller === 1){
+					next();
 				} else{
 					res.status(401).send({"status": 401, "message": "Unauthorized"});
 				};
-			});
-		};
-	};
+			} else{
+				res.status(401).send({"status": 401, "message": "Unauthorized"});
+			};
+		});
+	}
 }
 
 module.exports = {
 	check_user_token,
 	check_user_id,
+	isSeller,
+	check_writer_id,
 };
